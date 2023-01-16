fb94fcc2514703f7cfc09de4f9cedf2be75c4d7d
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-16 19:25:19 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Loading from './components/Loading/Loading';
+// import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+
+  const Home = React.lazy(() => import('./pages/Home/Home'));
+  // const NotFound = React.lazy(() => import('./components/error_404'));
+  // const ArticleList = React.lazy(() => import('./components/Article/ArticleList'));
+  // const ArticleCreate = React.lazy(() => import('./components/Article/ArticleCreate'));
+  // const ArticleUpdate = React.lazy(() => import('./components/Article/ArticleUpdate'));
+
+  return (
+    <div>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          {/* <Header /> */}
+          <ToastContainer
+            position="top-right"
+            autoClose={2500}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
