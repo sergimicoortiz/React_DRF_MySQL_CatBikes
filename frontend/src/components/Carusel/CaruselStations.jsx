18298ce69de09217097e77429e1366c4c54f34f4
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-24 17:22:10 +0100

    carusel END

--- frontend/src/components/Carusel/CaruselStations.jsx
+++ frontend/src/components/Carusel/CaruselStations.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useStations } from "../../hooks/useStations";
 import CaruselItem from "./caruselItem";
 import './CaruselStations.scss';
 
 
 const CaruselStations = () => {
 
-    const data = [
-        { img: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg', slug: 's' },
-        { img: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg', slug: 's' },
-        { img: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg', slug: 's' },
-        { img: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg', slug: 's' },
-        { img: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg', slug: 's' },
-    ]
+    const { stations } = useStations();
 
+    const data = stations.map(item => {
+        return { img: item.image, slug: item.slug };
+    })
     const carusel_items = data.map((item, index) =>
         <SwiperSlide key={index}><CaruselItem data={item} /></SwiperSlide>)
     return (
         <div>
+            <br />
             <Swiper
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
                 spaceBetween={50}
@@ -28,6 +26,7 @@ const CaruselStations = () => {
                 scrollbar={{ draggable: true }}>
                 {carusel_items}
             </Swiper>
+            <br />
         </div>
     )
 }
