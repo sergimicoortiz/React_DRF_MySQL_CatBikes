3a51af27335aac775f600da9abe563beceea1680
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-31 19:10:02 +0100

    issue #35 login/register

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,42 +1,12 @@
 import React from "react";
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import { useNavigate } from 'react-router-dom';
-import './LoginRegister.scss';
+import LoginRegisterForm from '../../components/LoginRegister/LoginRegisterForm';
 import { useUser } from "../../hooks/useUser";
-
 const Login = () => {
-
-    const navigate = useNavigate();
-    const validators = Yup.object().shape({
-        username: Yup.string().required('Username is required').min(3).max(15),
-        password: Yup.string().required().min(8, 'Password must have a least 8 characters'),
-    });
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({
-        resolver: yupResolver(validators)
-    });
-
-    const { useLogin } = useUser();
-
+    const { useLogin, errorsUser } = useUser();
     return (
-        <div className="login-page">
+        <div>
             <h1>LOGIN</h1>
-            <div className="form">
-                <form className="login-form" onSubmit={handleSubmit(useLogin)}>
-                    <input type="text" placeholder="Username" name="username" {...register('username')} />
-                    <div className="error">{errors.username?.message}</div>
-                    <input type="password" placeholder="Password" name="password" {...register('password')} />
-                    <div className="error">{errors.password?.message}</div>
-                    <button>login</button>
-                    <p className="message">Not registered? <a onClick={() => navigate('/register')}>Create an account</a></p>
-                </form>
-            </div>
+            <LoginRegisterForm sendData={(data) => useLogin(data)} errorsUser={errorsUser} />
         </div>
     )
 }
