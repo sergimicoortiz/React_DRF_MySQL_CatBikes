0cd01e3dff4f1cdd1281eae870529dc16e7a0083
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-02 20:39:39 +0100

    modal css fixed

--- frontend/src/components/MapStations/MarkerStation.jsx
+++ frontend/src/components/MapStations/MarkerStation.jsx
@@ -18,13 +18,16 @@ const MarkerStation = ({ station = {
 
     const customStyles = {
         content: {
-            top: '63%',
+            top: '50%',
             left: '50%',
             right: 'auto',
             bottom: 'auto',
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
         },
+        overlay: {
+            zIndex: '999',
+        }
     };
 
     const navigate = useNavigate();
