b3f46f4d9d7b0b2a33042f4f468a37cff2f69c48
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-02-02 15:46:29 +0100

    Change time Interval

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -24,7 +24,7 @@ export function UserContextProvider({ children }) {
                 } else {
                     sessionStorage.setItem("time", Number(1))
                 }
-            }, 10000);
+            }, 60000);
 
             UserService.GetUser()
                 .then(({ data, status }) => {
