8646eb0e094ff865806192822fdb0adc7bb7c5a4
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-07 18:46:48 +0100

    infinite scroll END

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.scss';
 import CaruselStations from '../../components/Carusel/CaruselStations';
+import InfiniteScrollStations from '../../components/InfiniteScrollStations/InfiniteScrollStations';
 
 const Home = () => {
     return (
         <div>
             <CaruselStations />
+            <InfiniteScrollStations />
         </div>
     )
 }
