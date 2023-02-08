1bd3a04b43c0550b7eb42d9b672a4d71fc04cf3a
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-08 21:16:25 +0100

    get all and one incident backend

--- backend/src/app/incidents/urls.py
+++ backend/src/app/incidents/urls.py
@@ -3,5 +3,6 @@ from .views import IncidentView
 
 urlpatterns = [
     path('incidents', IncidentView.as_view({"get": "get"})),
+    path('incidents/<str:slug>', IncidentView.as_view({"get": "get"})),
     path('incidents', IncidentView.as_view({"post": "post"})),
 ]
--- backend/src/app/incidents/views.py
+++ backend/src/app/incidents/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.permissions import (IsAuthenticated)
 from src.app.core.permissions import IsAdmin
 from .models import Incident
@@ -17,8 +18,15 @@ class IncidentView(viewsets.GenericViewSet):
             self.permission_classes = [IsAdmin]
         return super(IncidentView, self).get_permissions()
 
-    def get(self, request):
-        return Response({'a': 'a'})
+    def get(self, request, slug=None):
+        if slug:
+            incident = get_object_or_404(Incident.objects.all(), slug=slug)
+            serializer_one = IncidentSerializer(incident)
+            return Response(serializer_one.data)
+        incidents = Incident.objects.all()
+        serializer = IncidentSerializer(incidents, many=True)
+
+        return Response(serializer.data)
 
     def post(self, request):
         data = request.data['incident']
