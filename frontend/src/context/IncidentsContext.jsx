f7cf934ee78384ccb3cec40230bbbbfd0ef2f5c1
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-02-09 17:03:16 +0100

    Dashboard Update and Delete END

--- frontend/src/context/IncidentsContext.jsx
+++ frontend/src/context/IncidentsContext.jsx
@@ -7,7 +7,6 @@ const Context = React.createContext({})
 export function IncidentsContextProvider({ children }) {
     const { isAdmin } = useContext(UserContext);
     const [incidents, setIncidents] = useState([]);
-
     useEffect(function () {
         if (isAdmin) {
             IncidentsService.getAll()
@@ -15,7 +14,7 @@ export function IncidentsContextProvider({ children }) {
                     setIncidents(data)
                 })
         }
-    }, [setIncidents])
+    }, [setIncidents, isAdmin])
 
     return <Context.Provider value={{ incidents, setIncidents }}>
         {children}
