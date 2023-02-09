ed70ba3b8cc9b97c1e9cce420806124ef16d0051
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-09 19:51:10 +0100

    notification model and trigger AU incidence

---
+++ backend/sql/ClientNotificationTrigger.sql
@@ -0,0 +1,16 @@
+delimiter //
+CREATE TRIGGER incidents_incident_AU
+AFTER UPDATE ON incidents_incident
+FOR EACH ROW
+BEGIN
+IF NEW.status = 'in_progress' THEN
+INSERT INTO incidents_notification (body, user_id, seen, created_at, modified_at) VALUES (CONCAT('The incidence: ', OLD.slug, ' is in progress.'), OLD.user_id, FALSE, NOW(), NOW());
+END IF;
+IF NEW.status = 'resolved' THEN INSERT INTO incidents_notification (body, user_id, seen, created_at, modified_at) VALUES 
+(CONCAT('The incidence: ', OLD.slug, ' is resolved.'), OLD.user_id, FALSE, NOW(), NOW());
+END IF;
+END//
+delimiter ;
+
+
+-- INSERT INTO incidents_notification (body, user_id, seen, created_at, modified_at) VALUES ('test', 1, FALSE, NOW(), NOW());
