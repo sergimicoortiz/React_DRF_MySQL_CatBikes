c0d28ab38dab0e6f7eb16d47e1c54c6a83f5505b
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-02-09 17:46:54 +0100

    Guard bug F5 FIXED

--- frontend/src/services/guards/AdminGuard.jsx
+++ frontend/src/services/guards/AdminGuard.jsx
@@ -1,11 +1,32 @@
 import React, { useContext } from 'react'
 import { Navigate, Outlet } from "react-router-dom"
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import UserContext from '../../context/UserContext'
+import UserService from '../UserService'
 
 function AdminGuard() {
     const { isAdmin } = useContext(UserContext)
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    if (location['pathname'].split('/')[1]) {
+        const path_tmp = location['pathname'].split('/')[2];
+        sessionStorage.setItem("path", path_tmp)
+    }
+
+    if (!isAdmin) {
+        UserService.GetUser()
+            .then(({ data, status }) => {
+                if (status == 200) {
+                    if (data.user.types == "admin") {
+                        setTimeout(() => {
+                            navigate("/dashboard/" + sessionStorage.getItem("path"))
+                        }, 500);
+                    }
+                }
+            })
+    }
 
     return isAdmin ? <Outlet /> : <Navigate to="/home" />
 }
