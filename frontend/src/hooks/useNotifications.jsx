642ef638b8500dbe177157369d70b02000569329
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-02-10 16:51:40 +0100

    Clean code

--- frontend/src/hooks/useNotifications.jsx
+++ frontend/src/hooks/useNotifications.jsx
@@ -7,11 +7,8 @@ export function useNotifications() {
     const { notifications, setNotifications } = useContext(NotificationsContext);
 
     const useSeeNotification = useCallback((id) => {
-        console.log("asd")
         NotificationsService.UpdateSeen(id)
             .then(({ data, status }) => {
-                console.log("then")
-                console.log(data);
                 if (status === 200) {
                     setNotifications(notifications.filter(item => item.id !== id));
                     toast.info('Notification seen');
